246f54277f1aa9be4dcb4e683ed7b38f908dd119
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-05-12 00:27:19 +0200

    genes indexing

---
+++ genes_indexing.cpp
@@ -0,0 +1,25 @@
+#include "colored_kDataFrame.hpp"
+#include "algorithms.hpp"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    if (argc != 3) {
+        cerr << "run ./genesIndexing <fasta_file> <names_mape>" << endl;
+        exit(1);
+    }
+
+    string fasta_file = argv[1];
+    string names_file = argv[2];
+
+    int kSize = 25;
+    int chunk_size = 10000;
+
+    auto *kf = new kDataFrameMQF(kSize, 1);
+    colored_kDataFrame *ckf = kProcessor::index(kf, {{"k_size", kSize}}, fasta_file, chunk_size, names_file);
+    ckf->save(fasta_file);
+
+}
