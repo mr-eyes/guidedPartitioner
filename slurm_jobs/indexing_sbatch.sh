ef7b11e2aa8b84294c4cfb56fe0e40400327a7b3
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-05-12 03:17:48 +0200

    python instead of cpp

--- slurm_jobs/indexing_sbatch.sh
+++ slurm_jobs/indexing_sbatch.sh
@@ -51,7 +51,8 @@ cp /home/mhussien/guidedPartitioner/data/gencode.v34.transcripts.fa.names ./
 FASTA=gencode.v34.transcripts.fa
 NAMES=gencode.v34.transcripts.fa.names
 
-/usr/bin/time -v /home/mhussien/guidedPartitioner/build/genesIndexing ${FASTA} ${NAMES}
+# /usr/bin/time -v /home/mhussien/guidedPartitioner/build/genesIndexing ${FASTA} ${NAMES}
+/usr/bin/time -v python /home/mhussien/guidedPartitioner/genes_indexing.py ${FASTA} ${NAMES}
 
 # Remove reads
 rm -rf ${FASTA}
