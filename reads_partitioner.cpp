42924ab294cb2ede984d6debe9759a566dcec8d5
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-05-22 00:41:29 +0200

    writing to sqliteDB done

--- reads_partitioner.cpp
+++ reads_partitioner.cpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include "readClassifier.hpp"
 #include <cassert>
+#include "sqliteManager.hpp"
 
 using namespace std;
 
@@ -71,6 +72,11 @@ int main(int argc, char **argv)
     int kSize = 25;
     int no_of_sequences = 67954363;
 
+    // Create DB table
+    string sqlite_db = "genes_partitions.db";
+    auto *SQL = new SQLiteManager(sqlite_db);
+    SQL->create_reads_table();
+
     // kProcessor Index Loading
     std::cerr << "Loading kProcessor index ..." << std::endl;
     colored_kDataFrame *ckf = colored_kDataFrame::load(index_prefix);
@@ -87,14 +93,11 @@ int main(int argc, char **argv)
 
     ReadsClassifier classifier(ckf);
 
-    ofstream test_debug_file;
-    test_debug_file.open("guided_partitions.tsv");
-
     while (!PEReader->end())
     {
         std::chrono::high_resolution_clock::time_point _currentTime = std::chrono::high_resolution_clock::now();
         cerr << "Processing chunk " << ++current_chunk << "/" << no_chunks << "... ";
-
+        vector<tuple<int, string, string, string>> sqlite_chunk; // Buffer for holding Sqlite rows
         for (auto const &PE : *PEReader->next_chunk())
         {
             classifier.stats.n_fragments++;
@@ -110,14 +113,8 @@ int main(int argc, char **argv)
 
             if (flag_fragment == "unique")
             {
-
                 assert(partition_ids.size() == 1);
-
-                test_debug_file << "unique" << '\t';
-                test_debug_file << partition_ids[0] << '\t';
-                test_debug_file << PE.R1_name << '\t';
-                test_debug_file << PE.R1_seq << '\t';
-                test_debug_file << PE.R2_seq << '\n';
+                sqlite_chunk.emplace_back(make_tuple(partition_ids[0], PE.R1_name, PE.R1_seq, PE.R2_seq));
             }
             else if (flag_fragment == "ambiguous")
             {
@@ -126,11 +123,7 @@ int main(int argc, char **argv)
 
                 for (const auto &pID : partition_ids)
                 {
-                    test_debug_file << "ambiguous" << '\t';
-                    test_debug_file << pID << '\t';
-                    test_debug_file << PE.R1_name << '\t';
-                    test_debug_file << PE.R1_seq << '\t';
-                    test_debug_file << PE.R2_seq << '\n';
+                    sqlite_chunk.emplace_back(make_tuple(pID, PE.R1_name, PE.R1_seq, PE.R2_seq));
                 }
             }
             else if (flag_fragment == "chimeric")
@@ -138,6 +131,57 @@ int main(int argc, char **argv)
                 continue;
             }
         }
+
+        // --------------------------------------------------------------------------------
+        //                                      SQLITE Insertion                          |
+        // --------------------------------------------------------------------------------
+
+        sqlite3_mutex_enter(sqlite3_db_mutex(SQL->db.db_));
+        char *errorMessage;
+        sqlite3_exec(SQL->db.db_, "PRAGMA synchronous=OFF", nullptr, nullptr, &errorMessage);
+        sqlite3_exec(SQL->db.db_, "PRAGMA count_changes=OFF", nullptr, nullptr, &errorMessage);
+        sqlite3_exec(SQL->db.db_, "PRAGMA journal_mode=MEMORY", nullptr, nullptr, &errorMessage);
+        sqlite3_exec(SQL->db.db_, "PRAGMA temp_store=MEMORY", nullptr, nullptr, &errorMessage);
+        sqlite3_exec(SQL->db.db_, "BEGIN TRANSACTION", nullptr, nullptr, &errorMessage);
+        char const *szSQL = "INSERT INTO reads (gene_id, read_id, PE_seq1, PE_seq2) VALUES (?,?,?,?);";
+        sqlite3_stmt *stmt;
+        SQL->rc = sqlite3_prepare(SQL->db.db_, szSQL, -1, &stmt, nullptr);
+
+        if (SQL->rc == SQLITE_OK) {
+
+            for (auto &row : sqlite_chunk) {
+                int geneID = get<0>(row);
+                char const *readID = get<1>(row).c_str();
+                char const *R1_seq = get<2>(row).c_str();
+                char const *R2_seq = get<3>(row).c_str();
+
+
+                sqlite3_bind_int(stmt, 1, geneID);
+                sqlite3_bind_text(stmt, 2, readID, strlen(readID), nullptr);
+                sqlite3_bind_text(stmt, 3, R1_seq, strlen(R1_seq), nullptr);
+                sqlite3_bind_text(stmt, 4, R2_seq, strlen(R2_seq), nullptr);
+                
+
+                int retVal = sqlite3_step(stmt);
+                if (retVal != SQLITE_DONE) {
+                    printf("Commit Failed! %d\n", retVal);
+                }
+
+                sqlite3_reset(stmt);
+            }
+            sqlite3_exec(SQL->db.db_, "COMMIT TRANSACTION", NULL, NULL, &errorMessage);
+            sqlite3_finalize(stmt);
+
+        } else {
+            fprintf(stderr, "SQL error: %s\n", SQL->zErrMsg);
+            sqlite3_free(SQL->zErrMsg);
+        }
+        sqlite3_mutex_leave(sqlite3_db_mutex(SQL->db.db_));
+
+        // --------------------------------------------------------------------------------
+        //                                      Done Insertion                            |
+        // --------------------------------------------------------------------------------
+
 
         cerr << "Done in " << time_diff(_currentTime) << endl;
     }
