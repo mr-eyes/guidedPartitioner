3f0a113a05542f8238630151ef1eddc9d23b6111
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-05-22 03:08:16 +0200

    dumping job

---
+++ slurm_jobs/dumping_sbatch.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+#! /bin/bash -login
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mabuelanin@gmail.com
+#SBATCH -p bmm
+#SBATCH -J genes_partitions_dumping
+#SBATCH --time=3:00:00
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 32
+#SBATCH --mem=10gb
+#SBATCH --output=slurm_%x.%j.out
+#SBATCH --error=slurm_%x.%j.err
+
+
+# activate conda in general (MUST BE PLACE BEFORE ANYTHING)
+. /home/mhussien/miniconda3/etc/profile.d/conda.sh
+conda activate omnigraph
+
+
+# exit when any command fails
+set -e
+set -o nounset
+set -o errexit
+set -x
+set -euox pipefail
+
+
+# make a directory specific to user and job
+export MYTMP=/scratch/${USER}/slurm_${SLURM_JOB_NAME}_${SLURM_JOBID}
+mkdir -p "$MYTMP"
+
+# force clean it up
+function cleanup() {
+    rm -rf "$MYTMP";
+}
+
+trap cleanup EXIT
+
+# ~~~~~~~~~~~~~~~~~~~~~ START Job ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+cd "$MYTMP"
+
+## Copy raw data
+cp /home/mhussien/guidedPartitioner/results/genes_partitions.db ./
+cp /home/mhussien/guidedPartitioner/dump_partitions.py ./
+
+
+############################## (1) START DUMPING ####################################
+
+DB=genes_partitions.db
+IDX_PREFIX=/home/mhussien/guidedPartitioner/results/mouse_index/idx_gencode.vM25.transcripts.fa
+NO_CORES=32
+
+/usr/bin/time -v dump_partitions.py ${DB} ${IDX_PREFIX} ${NO_CORES}
+
+
+rm -rf ${DB}
+rm -rf dump_partitions.py
+
+
+############################## DONE #######################################
+
+
+########################### Move scratch to home dir ###################
+
+
+cp -r $MYTMP "$SLURM_SUBMIT_DIR"
+scontrol show job $SLURM_JOB_ID     # Print out final statistics about resource uses before job exits
+
+# Print out values of the current jobs SLURM environment variables
+env | grep SLURM
+
+scontrol show job ${SLURM_JOB_ID}     # Print out final statistics about resource uses before job exits
+
+sstat --format 'JobID,MaxRSS,AveCPU' -P ${SLURM_JOB_ID}.batch
