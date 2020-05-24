b5b768859caae8715a695fc40bbb5b4c391a7a4a
Mohamed Abuelanin <mohamed.abuelanin@gmail.com>
2020-05-24 08:11:56 +0200

    missing parameter

--- extract_interlaced.py
+++ extract_interlaced.py
@@ -1,6 +1,7 @@
 import sys
 import gzip
 
+
 def auto_open(file_name, mode, encoding='utf8'):
     if file_name[-3:] == ".gz":
         return gzip.open(file_name, mode + 't', encoding=encoding)
@@ -15,8 +16,8 @@ else:
     fasta_file = sys.argv[1]
 
 
-file1_name = fasta_file.replace(".fa") + "_1.fa"
-file2_name = fasta_file.replace(".fa") + "_2.fa"
+file1_name = fasta_file.replace(".fa", '') + "_1.fa"
+file2_name = fasta_file.replace(".fa", '') + "_2.fa"
 
 with auto_open(fasta_file, 'r') as FASTA_READER, open(file1_name, 'w') as FA1, open(file2_name, 'w') as FA2:
     for line in FASTA_READER:
